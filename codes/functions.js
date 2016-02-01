49b2f62b89592ec2a9946d0943e03de61a9c3cd6
muscem <muscem1@hotmail.com>
2016-02-01 18:28:26 +0200

    İlk dosya yükleme
    Dosyalar ilk defa yükleniyor

---
+++ codes/functions.js
@@ -0,0 +1,25 @@
+//$(function() {
+
+function getPage(page){
+	$.ajax({
+		type: "POST",
+		url: page,
+		data: { c: "s" },
+		dataType: "html",
+		beforeSend: function( xhr ) {
+			//
+		}
+	})
+	.done(function( msg ) {
+		$("body").html( msg );
+	});
+}
+function openPageOnLoad(){
+	getPage("http://www.tayangu.com/mobil/pages/index.html");
+}
+function changePage(page){
+	getPage("http://www.tayangu.com/mobil/pages/"+page);
+}
+
+
+//});
